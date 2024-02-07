dbb406f0bbc935143af98ab1ec691fc939ec9ff6
Adrian Birta <adrianbirta@gmail.com>
2024-02-07 10:03:07 +0200

    Add React Refresh feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './styles.css';
 import IMAGE from './react.png';
 import LOGO from './logo.svg';
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
     return <>
-        <h1>React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
+        <h1>Edited React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={IMAGE} alt="React Logo" width="250" height="200"/>
         <img src={LOGO} alt="React Logo" width="250"/>
+        <ClickCounter />
     </>
 
 }
