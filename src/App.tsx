f9a1a3d6fd62b65a12bf63d7834f186e0a437760
Adrian Birta <adrianbirta@gmail.com>
2024-02-07 15:11:15 +0200

    Add Prettier to project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import './styles.css';
-import IMAGE from './react.png';
-import LOGO from './logo.svg';
-import { ClickCounter } from './ClickCounter';
+import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-    return <>
-        <h1>Edited React Typescript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
-        <img src={IMAGE} alt="React Logo" width="250" height="200" />
-        <img src={LOGO} alt="React Logo" width="250" />
-        <ClickCounter />
+  return (
+    <>
+      <h1>
+        Edited React Typescript Webpack Starter Template -{' '}
+        {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width="250" height="200" />
+      <img src={LOGO} alt="React Logo" width="250" />
+      <ClickCounter />
     </>
+  )
 }
